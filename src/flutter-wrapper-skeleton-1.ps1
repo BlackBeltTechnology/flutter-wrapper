de40ae2ca09e8dda993f4676cc20ca0c6de9c9e2
Robert Csakany <csakany.robert@gmail.com>
2020-11-11 23:22:53 +0100

    Fixing flutter directory in powershell

--- src/flutter-wrapper-skeleton-1.ps1
+++ src/flutter-wrapper-skeleton-1.ps1
@@ -4,6 +4,6 @@ $flutter = "$scriptDir\.flutter\bin\flutter"
 
 if (!(Test-Path $flutterDir)) {
   Write-Output "Flutter not found, installing..."
-  git clone https://github.com/flutter/flutter.git -b beta --depth 1 $scriptDir\flutter
+  git clone https://github.com/flutter/flutter.git -b beta --depth 1 $scriptDir\.flutter
   $handle = Start-Process 'cmd' -ArgumentList "/c $flutter doctor" -NoNewWindow -PassThru -Wait
 }
