98e29c6fff20be4a14cfd5420a66248fcd081928
Robert Csakany <csakany.robert@gmail.com>
2020-11-16 10:52:40 +0100

    Release changes

--- dist/flutterw.ps1
+++ dist/flutterw.ps1
@@ -12,12 +12,12 @@ if (!(Test-Path $chromewsh)) {
   $Content = [System.Convert]::FromBase64String("IyEvYmluL3NoCmlmIFsgImB1bmFtZWAiID0gIkRhcndpbiIgXTsgdGhlbgogIENIUk9NRV9CSU49L0FwcGxpY2F0aW9ucy9Hb29nbGVcIENocm9tZS5hcHAvQ29udGVudHMvTWFjT1MvR29vZ2xlXCBDaHJvbWUKZWxpZiBbICJgdW5hbWVgIiA9ICJMaW51eCIgXTsgdGhlbgogIENIUk9NRV9CSU49JCh3aGljaCBnb29nbGUtY2hyb21lKQplbHNlCiAgZWNobyAiVW5zdXBwb3J0ZWQgT1MiCiAgZXhpdCAxCmZpCgplY2hvICJDaHJvbWU6ICRDSFJPTUVfQklOIgppZiBbICEgLWYgIiR7Q0hST01FX0JJTn0iIF07IHRoZW4KICBlY2hvICJHb29nbGUgY2hyb21lIG5vdCBmb3VuZCIKICBleGl0IDEKZmkKCiNleGUgYyBiYXNoIC1jICJleGVjICR7Q0hST01FX0JJTn0gLS1kaXNhYmxlLXdlYi1zZWN1cml0eSAtLXVzZXItZGF0YS1kaXI9XCIuY2hyb21lXCIgJCoiCgoiJENIUk9NRV9CSU4iIC0tZGlzYWJsZS13ZWItc2VjdXJpdHkgLS11c2VyLWRhdGEtZGlyPSIuY2hyb21lIiAkKg==")
   Set-Content -Path $chromewsh -Value $Content -Encoding Byte
 }
-$chromewps1 = "$scriptDir\chromew.ps1"
-if (!(Test-Path $chromewps1)) {
+$chromewcmd = "$scriptDir\chromew.cmd"
+if (!(Test-Path $chromewcmd)) {
   $Content = [System.Convert]::FromBase64String("ImM6XFByb2dyYW0gRmlsZXMgKHg4NilcR29vZ2xlXENocm9tZVxBcHBsaWNhdGlvblxjaHJvbWUuZXhlIiAtLWRpc2FibGUtd2ViLXNlY3VyaXR5IC0tdXNlci1kYXRhLWRpcj0iLmNocm9tZSIgJSo=")
-  Set-Content -Path $chromewps1 -Value $Content -Encoding Byte
+  Set-Content -Path $chromewcmd -Value $Content -Encoding Byte
 }
-$env:CHROME_EXECUTABLE="$scriptDir\chromew.ps1"
+$env:CHROME_EXECUTABLE="$scriptDir\chromew.cmd"
 
 $handle = Start-Process 'cmd' -ArgumentList "/c $flutter $args" -NoNewWindow -PassThru -Wait
 
